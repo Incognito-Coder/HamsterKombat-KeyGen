c98f245241c616175efebe65a764fa20e3259039
Igor Baluev <s-el-igor@yandex.ru>
2024-08-27 20:01:26 +0500

    Сделал код понятным и читаемым. Убрал лишнее объявление переменной и избавился от объявления другой переменной снаружи цикла

--- keygen.py
+++ keygen.py
@@ -1,5 +1,4 @@
 import time
-import random
 import uuid
 import aiohttp
 import asyncio
@@ -9,11 +8,10 @@ import random
 from loguru import logger
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base
-DATABASE_URL='mysql+pymysql://root:123123@192.168.10.28/123123'
+DATABASE_URL = 'mysql+pymysql://root:123123@192.168.10.28/123123'
 Base = declarative_base()
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
-session = Session
 write_to_database = False
 
 
@@ -31,11 +29,7 @@ Base.metadata.create_all(engine)
 os.system('title HamsterKombat Games Code Generator')
 
 logger.remove()
-logger.add(sink=sys.stdout,
-           format="<white>{time:YYYY-MM-DD HH:mm:ss}</white>"
-           " | <level>{level: <8}</level>"
-           " | <cyan><b>{line}</b></cyan>"
-           " - <white><b>{message}</b></white>")
+logger.add(sink=sys.stdout, format="<white>{time:YYYY-MM-DD HH:mm:ss}</white>"" | <level>{level: <8}</level>"" | <cyan><b>{line}</b></cyan>"" - <white><b>{message}</b></white>")
 logger = logger.opt(colors=True)
 
 games = {
@@ -100,6 +94,7 @@ def generate_client_id():
 def generate_event_id():
     return str(uuid.uuid4())
 
+
 def insert_to_db(promo_code):
     with Session() as session:
         new_code = Record(user_id=None, content=promo_code, date_sent=None)
@@ -157,7 +152,8 @@ async def get_promo_code(app_token: str, promo_id: str, file: str, event_timeout
             except Exception as error:
                 logger.error(f"Error while getting promo code: {error}")
             attempts += 1
-            logger.info(f"Attempt {attempts} was successful | Sleep {event_timeout}s before {attempts + 1} attempt to get promo code")
+            logger.info(f"Attempt {attempts} was successful | Sleep {event_timeout}s "
+                        f"before {attempts + 1} attempt to get promo code")
             await asyncio.sleep(delay=event_timeout)
     logger.warning(f"Promo code not found out of {max_attempts} attempts")
     input("Press enter to exit")
@@ -165,9 +161,7 @@ async def get_promo_code(app_token: str, promo_id: str, file: str, event_timeout
 
 
 if __name__ == '__main__':
-    for key, value in games.items():
-         game_choice = random.randint(1, key)
-         choisen_game = games[game_choice]
+    game_choice = random.choice(list(games.keys()))
     if game_choice in games:
         max_attemps = 30
         loop = asyncio.new_event_loop()
@@ -175,14 +169,13 @@ if __name__ == '__main__':
         try:
             while True:
                 if max_attemps:
-                    logger.info(f"Генерируется токен для игры {choisen_game['name']}")
+                    logger.info(f"Генерируется токен для игры {games[game_choice]['name']}")
                     loop.run_until_complete(get_promo_code(app_token=games[game_choice]['appToken'],
                                                            promo_id=games[game_choice]['promoId'],
                                                            file=games[game_choice]['short'],
                                                            max_attempts=int(max_attemps),
                                                            event_timeout=20))
-                    game_choice = random.randint(1, key)
-                    choisen_game = games[game_choice]
+                    game_choice = random.choice(list(games.keys()))
                 else:
                     loop.run_until_complete(get_promo_code(app_token=games[game_choice]['appToken'],
                                                            promo_id=games[game_choice]['promoId'],
